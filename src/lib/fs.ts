44cd611d8c098dc61a660cee45ed2bd8a8ad4b7f
Dennis Schoepf <dennis@schoepf.co>
2020-11-13 14:08:02 +0100

    Remove unused function

--- src/lib/fs.ts
+++ src/lib/fs.ts
@@ -24,7 +24,3 @@ export async function readMultipleFiles(filePaths: string[]): Promise<File[]> {
     fileContentBuffer,
   }));
 }
-
-export function getFilename(filePath: string): string {
-  return path.basename(filePath, path.extname(filePath));
-}
